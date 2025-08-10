1d1795bd352dda772d48847ec6eee4c2005ff2a9
sushilnamberdar <sushilnamberdar000@gmail.com>
2025-08-10 16:43:11 +0530

    update

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -3,22 +3,20 @@ const { getCart, addToCart, removeFromCart, updateQuantity, clearCart } = requir
 const { authMiddleware } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
-// All routes require authentication
-router.use(authMiddleware);
 
 // Get cart
-router.get('/', getCart);
+router.get('/', authMiddleware, getCart);
 
 // Add to cart
-router.post('/add', addToCart);
+router.post('/add', authMiddleware, addToCart);
 
 // Clear entire cart for a specific user
-router.delete('/clear', clearCart);
+router.delete('/clear',  authMiddleware, clearCart);
 
 // Update quantity
-router.put('/:productId/quantity', updateQuantity);
+router.put('/:productId/quantity', authMiddleware, updateQuantity);
 
 // Remove from cart
-router.delete('/:productId', removeFromCart);
+router.delete('/:productId', authMiddleware, removeFromCart);
 
 module.exports = router;
