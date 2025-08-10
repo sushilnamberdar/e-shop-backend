0b7fa1a4f2f545a18a4850e378506401d78f03b9
sushilnamberdar <sushilnamberdar000@gmail.com>
2025-08-10 16:53:48 +0530

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -104,7 +104,7 @@ exports.register = async (req, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
     res.cookie('token', token, {
       httpOnly: true,
-      sameSite: 'lax',
+      sameSite: 'none',
       secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
@@ -136,7 +136,7 @@ exports.login = async (req, res) => {
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
   res.cookie('token', token, {
     httpOnly: true,
-    sameSite: 'lax',
+    sameSite: 'none',
     secure: true,
     maxAge: 7 * 24 * 60 * 60 * 1000
   });
